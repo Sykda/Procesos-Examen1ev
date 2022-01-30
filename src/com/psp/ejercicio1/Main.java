1c9052ec0583629335e27142ab01804d75035095
Sykda <robermontes93@gmail.com>
2022-01-30 17:17:58 +0100

    commit

---
+++ src/com/psp/ejercicio1/Main.java
@@ -0,0 +1,13 @@
+package com.psp.ejercicio1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            Thread dalmata = new Thread(new Dalmata(i));
+            dalmata.start();
+        }
+
+    }
+}
